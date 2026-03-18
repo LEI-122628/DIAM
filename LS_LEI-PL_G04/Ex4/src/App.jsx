a11b90fce170adecf7b434736094b5f677b061f7
Tomas Salvador <tomas.f.l.salvador@gmail.com>
2026-03-18 13:47:56 +0000

    Gallery of companies

--- LS_LEI-PL_G04/Ex4/src/App.jsx
+++ LS_LEI-PL_G04/Ex4/src/App.jsx
@@ -1,19 +1,21 @@
 import Header from './components/Header'
-import Navbar from './components/Navbar'
+import NavBar from './components/NavBar'
 import Slideshow from './components/Slideshow'
 import Countdown from './components/Countdown'
 import About from './components/About'
 import Timeline from './components/Timeline'
 import Footer from './components/Footer'
 import './App.css'
+import Gallery from './components/Gallery'
 
 function App() {
   return (
     <>
       <Header />
-      <Navbar />
+      <NavBar />
       <Slideshow />
       <Countdown />
+      <Gallery />
       <div className="content-wrapper">
         <About />
         <Timeline />
